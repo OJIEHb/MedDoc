dfdbbd084a7bf11c99d4f662126657d18bdc3ed8
Andrew Heiko <geiko2015@gmail.com>
2018-09-24 09:07:49 +0300

    15

--- src/app/components/templates/uterus/uterus-template/uterus-template.component.ts
+++ src/app/components/templates/uterus/uterus-template/uterus-template.component.ts
@@ -26,4 +26,12 @@ export class UterusTemplateComponent {
     this.selectedItems[e.target.id] = e.target.selectedIndex;
   }
 
+  onCheckboxChange() {
+    this.templateData.eggDetails = '';
+    this.templateData.eggDetails += this.templateData.eggDetails1 ? 'В порожнині матки визначається плідне яйце d {eggDetails5} мм, не деформоване, форма не змінена, візуалізується плід КТР {eggDetails6} мм; серцебиття визначається, рух визначається; хоріон розташован по {eggDetails7}. ' : '';
+    this.templateData.eggDetails += this.templateData.eggDetails2 ? 'В порожнині матки визначається плідне яйце d1 {eggDetails8} мм, деформоване, форма змінена, візуалізується плід КТР {eggDetails9} мм; серцебиття не визначається, рух не визначається; хоріон розташован по {eggDetails10}. ' : '';
+    this.templateData.eggDetails += this.templateData.eggDetails3 ? 'В порожнині матки визначається друге плідне яйце d2 {eggDetails11} мм, {eggDetails12} деформоване, форма {eggDetails13} змінена, візуалізується плід КТР {eggDetails14} мм; серцебиття {eggDetails15} визначається, рух {eggDetails16} визначається; хоріон розташован по {eggDetails17}. ' : '';
+    this.templateData.eggDetails += this.templateData.eggDetails4 ? 'В порожнині матки визначається плідне яйце d {eggDetails18} мм, {eggDetails19} деформоване, форма {eggDetails20} змінена, хоріон розташован по {eggDetails21}. {eggDetails22}' : '';
+  }
+
 }
